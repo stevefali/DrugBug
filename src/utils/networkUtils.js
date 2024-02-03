3431ed96bebe545618884cd5634cf9605f72a2ac
stevefali <stevefaliszewski@gmail.com>
2024-02-03 04:17:37 -0800

    Install axios and use it to fetch medication data from the server on the home page

---
+++ src/utils/networkUtils.js
@@ -0,0 +1,16 @@
+const BASE = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}`;
+
+export const getUserMedicationsEndpoint = (userId) =>
+  `${BASE}/user/medications/${userId}`;
+
+export const putModifyMedicationEndpoint = (medicationId) =>
+  `${BASE}/medication/${medicationId}`;
+
+export const putModifyDoseEndpoint = () => `${BASE}/dose/update`;
+
+export const postAddMedicationEndpoint = () => `${BASE}/medication/add`;
+
+export const deleteDoseEndpoint = (doseId) => `${BASE}/dose/${doseId}`;
+
+export const deleteMedicationEndpoint = (medicationId) =>
+  `${BASE}/medication/${medicationId}`;
