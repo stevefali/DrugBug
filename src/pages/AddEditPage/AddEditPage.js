f9695c479d75e49133e1008fe3ddae845156f3ac
stevefali <stevefaliszewski@gmail.com>
2024-02-03 23:55:11 -0800

    Add MedicineForm and DoseForm to add/edit page

--- src/pages/AddEditPage/AddEditPage.js
+++ src/pages/AddEditPage/AddEditPage.js
@@ -1,10 +1,14 @@
 import Container from "react-bootstrap/Container";
+import MedicineForm from "../../components/MedicineForm/MedicineForm";
+import DoseForm from "../../components/DoseForm/DoseForm";
 import "./AddEditPage.scss";
 
 const AddEditPage = () => {
   return (
     <Container>
       <h1>Add New Medication</h1>
+      <MedicineForm />
+      <DoseForm />
     </Container>
   );
 };
